985296f9405ffb04564688baaaa1c4cade51760e
Yaroslav <ia.iudin@click-group.ru>
2023-07-19 18:47:29 +0300

    начальные настройки

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ConfigProvider } from 'antd';
+import ru_RU from 'antd/lib/locale/ru_RU';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter basename='/'>
+    <ConfigProvider locale={ru_RU}>
+      <App />
+    </ConfigProvider>
+  </BrowserRouter>,
+);
